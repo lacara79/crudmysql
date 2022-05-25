fa916c61ebfa003fef9f5b5fc1a65a80c0ecd6c0
Luiz Alexandre da Costa Araujo <48302746+lacara79@users.noreply.github.com>
2022-05-25 11:53:55 -0300

    Add files via upload

---
+++ aplicativo/select.php
@@ -0,0 +1,26 @@
+<?php 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Method: POST, GET, PUT, PATCH, DELETE,OPTION');
+header('Access-Control-Allow-Headers: token, Content-Type');
+header('Access-Control-Max-Age: 1728000'); 
+header('Access-Length: 0');
+header('Content-Type: text/plain');
+
+	$pdo = new PDO("mysql:host=localhost;dbname=aula", "root", ""); 
+
+    $sql = $pdo->query("select * from aluno");
+
+    //Para Leitura
+    while($row = $sql->fetch()){
+        //echo $row["codigo"];
+
+        $alunos[] = array(
+            "id" => $row['codigo'],	
+            "nome"=> $row['nome'],
+            "email"=> $row['email']
+          
+            );           
+    }
+
+    echo json_encode($alunos);
+?>
